ff6c48edde99bda20c4bc8a1f5830e7741709522
Techcable <Techcable@outlook.com>
2014-11-22 18:31:56 -0700

    Finish UUIDPlayer

--- compat-1.6.x/src/main/java/techcable/minecraft/factionsapi/compat/v1_6_x/UUIDPlayer.java
+++ compat-1.6.x/src/main/java/techcable/minecraft/factionsapi/compat/v1_6_x/UUIDPlayer.java
@@ -2,6 +2,9 @@ package techcable.minecraft.factionsapi.compat.v1_6_x;
 
 import org.bukkit.OfflinePlayer;
 
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+
 import techcable.minecraft.factionsapi.FRank;
 
 import lombok.*;
@@ -16,12 +19,20 @@ public class UUIDPlayer extends techcable.minecraft.factionsapi.FPlayer {
 
 	@Override
 	public UUIDFaction getFaction() {
-		return null;
+		return getFactions().getUUIDFaction(getBacking().getFaction());
 	}
 
 	@Override
 	public FRank getRank() {
-		return null;
+		switch (getBacking().getRole()) {
+		case ADMIN : return FRank.OWNER;
+		case MODERATOR : return FRank.MODERATOR;
+		case NORMAL : return FRank.MEMBER;
+		default : return FRank.MEMBER; //Worst case scenerio get less permissions then should have
+		}
 	}
-
+	
+	public FPlayer getBacking() {
+		return FPlayers.getInstance().getByOfflinePlayer(getPlayer());
+	}
 }
